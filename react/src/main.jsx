cdd3a8d1061544f6e5a3320a15bd5f1f573a2811
Med El Khalil <BouroubaMohamed004@gmail.com>
2023-04-26 18:54:56 +0100

    new postcss config

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ContextProvider } from "./contexts/ContextProvider";
-import "./index.css";
 import router from "./router.jsx";
+
+// Import index.css for the home page only
+if (window.location.pathname === "/") {
+  import("./index2.css");
+}
+
+// Import index2.css for /admin and /doctor pages
+if (window.location.pathname.startsWith("/admin") || window.location.pathname.startsWith("/doctor")) {
+  import("./index.css");
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- react/src/views/home/Home.tsx
+++ react/src/views/home/Home.tsx
@@ -85,73 +85,13 @@ export default function Home() {
             </span>
             <span>
             {userToken ? (
-               <Menu as="div" className="relative inline-block text-left">
-               <div>
-                 <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                   {currentUser.name}
-                   <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-                 </Menu.Button>
-               </div>
-         
-               <Transition
-                 as={Fragment}
-                 enter="transition ease-out duration-100"
-                 enterFrom="transform opacity-0 scale-95"
-                 enterTo="transform opacity-100 scale-100"
-                 leave="transition ease-in duration-75"
-                 leaveFrom="transform opacity-100 scale-100"
-                 leaveTo="transform opacity-0 scale-95"
-               >
-                 <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                   <div className="py-1">
-                     <Menu.Item>
-                       {({ active }) => (
-                         <a
-                           href="/profile"
-                           className={classNames(
-                             active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                             'block px-4 py-2 text-sm'
-                           )}
-                         >
-                           Profile
-                         </a>
-                       )}
-                     </Menu.Item>
-                     {currentUser.usertype === 'donor' && (
-           <Menu.Item>
-             {({ active }) => (
-               <a
-                 href="/donate"
-                 className={classNames(
-                   active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                   'block px-4 py-2 text-sm'
-                 )}
-               >
-                 Donate now
-               </a>
-             )}
-           </Menu.Item>
-         )}
-                     <form method="POST" action="#" className="py-1">
-                       <Menu.Item>
-                         {({ active }) => (
-                           <button onClick={handleLogout} className={classNames(
-                             active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                             'block w-full px-4 py-2 text-left text-sm'
-                           )} aria-label="Logout">Logout</button>
-                         )}
-                       </Menu.Item>
-                     </form>
-                   </div>
-                 </Menu.Items>
-               </Transition>
-             </Menu>
+              <RouteLink to={"/profile"}>
+                {currentUser.name}
+              </RouteLink>
             ) : (
-              <div>
-              <button className="border border-gray-400 px-4 py-2">
-                <RouteLink to={'/login'}>Log In</RouteLink>
-              </button>
-            </div>
+              <RouteLink to={"/login"}>
+              <span>Login</span>
+            </RouteLink>
             )}
             </span>
           </div>
